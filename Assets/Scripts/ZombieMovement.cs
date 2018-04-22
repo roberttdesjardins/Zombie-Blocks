e3c6b041b28592bdff8f05dd2f7e64e82b8fb18f
Robert Desjardins <roberttdesjardins@gmail.com>
2018-04-22 12:03:26 -0700

    Added movement to zombie, navmesh agent and movement script

---
+++ Assets/Scripts/ZombieMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieMovement : MonoBehaviour {
+
+	Transform player;
+	UnityEngine.AI.NavMeshAgent nav;
+
+	void Awake() {
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+
+		nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		nav.SetDestination (player.position);
+	}
+}
